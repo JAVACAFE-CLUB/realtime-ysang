18d3579414cdb715bc267bdef8173d6fefe49bdc
ysang989 <67964576+ysang989@users.noreply.github.com>
2025-08-06 23:21:29 +0900

    📦Chore:프로젝트 세팅 (#1)

---
+++ src/test/java/javacafe/realtimesangyeon/RealtimesangyeonApplicationTests.java
@@ -0,0 +1,13 @@
+package javacafe.realtimesangyeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimesangyeonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
