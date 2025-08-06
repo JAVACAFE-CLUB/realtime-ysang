f3eef91397bfc481afe5e43da877fdae8bb9caee
ysang989 <67964576+ysang989@users.noreply.github.com>
2025-08-06 23:05:14 +0900

    📦Chore:프로젝트 세팅

---
+++ src/main/java/javacafe/realtimesangyeon/RealtimesangyeonApplication.java
@@ -0,0 +1,13 @@
+package javacafe.realtimesangyeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimesangyeonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtimesangyeonApplication.class, args);
+    }
+
+}
